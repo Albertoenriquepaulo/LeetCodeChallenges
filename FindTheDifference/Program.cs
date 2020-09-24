707eb133083d06f0b4e2a013c07d9d5845d8b7bc
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-09-25 00:56:05 +0200

    FindTheDifference

---
+++ FindTheDifference/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FindTheDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcd";
+            string t = "abcde";
+            Solution sol = new Solution();
+            var char1 = sol.FindTheDifference(s, t);
+        }
+    }
+    public class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < t.Length; j++)
+                {
+                    if (s[i] == t[j])
+                    {
+                        t = t.Remove(j, 1);
+                        break;
+                    }
+                }
+            }
+            return t[0];
+        }
+    }
+}
